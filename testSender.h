f0010660ae9e7d2e4f37b554e2c7437e6ebbd2b1
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-07-13 20:01:54 +0530

    Update testSender.h

--- testSender.h
+++ testSender.h
@@ -4,7 +4,6 @@
 //To check if the sender reads data from input file
 void TestCheckSenderReadsDatafromInputFile()
 {
-  SenderStatus status;
   std::vector<float> temperature;
   std::vector<float> stateOfCharge;
   int numberOfSamples = 0;
