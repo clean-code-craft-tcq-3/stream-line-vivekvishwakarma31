0a2f06181a3df0c7e0c9c0c69c88cc46731cce8e
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-07-13 19:50:37 +0530

    Update testSender.h

--- testSender.h
+++ testSender.h
@@ -1,1 +1,12 @@
 #include "sender.h"
+#include <assert.h>
+
+//To check if the sender reads data from input file
+void TestCheckSenderReadsDatafromInputFile()
+{
+  SenderStatus status;
+  std::vector<float> temperature;
+  std::vector<float> stateOfCharge;
+  int numberOfSamples = 0;
+  assert(ReadBMSParametersFromInputFile(temperature, stateOfCharge, numberOfSamples) == SenderStatus::SUCCESS); 
+}
