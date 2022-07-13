0201c307b9d260c3a06470188c48105f9c0fe044
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-07-13 19:41:14 +0530

    Create sender.cpp

---
+++ sender.cpp
@@ -0,0 +1,53 @@
+#include "sender.h"
+using namespace std;
+
+SenderStatus ReadBMSParametersFromInputFile(std::vector<float> &temperature, std::vector<float> &stateOfCharge, int &numberOfSamples)
+{
+    float fTempVal, fChargeRateVal = 0;
+    string TempVal, ChargeRateVal;
+    string line;
+    SenderStatus readingFileStatus;
+    ifstream inputFileStream;
+    inputFileStream.open("./sample2.txt");
+    char *token;
+    if (!inputFileStream)
+    {
+        readingFileStatus = SenderStatus::FAILURE;
+    }
+    else
+    {
+        while (inputFileStream.peek() != EOF)
+        {
+            getline(inputFileStream, TempVal, ',');
+            getline(inputFileStream, ChargeRateVal, '\n');
+            fTempVal = std::stof(TempVal);
+            fChargeRateVal = std::stof(ChargeRateVal);
+            temperature.push_back(fTempVal);
+            stateOfCharge.push_back(fChargeRateVal);
+            numberOfSamples++;
+        }
+        readingFileStatus = SenderStatus::SUCCESS;
+    }
+    inputFileStream.close();
+    return readingFileStatus;
+}
+
+void printBMSParamsOnConsole(std::vector<float> &temperature, std::vector<float> &stateOfCharge, int &numberOfSamples)
+{
+    for (int i = 0; i<numberOfSamples; i++)
+    {
+        printf(" Temperature:%.2f, StateofCharge:%.2f \n", (temperature[i]), (stateOfCharge[i]));  
+    }
+    return;
+
+}
+
+SenderStatus SendBMSParameters()
+{
+    std::vector<float> temperature;
+    std::vector<float> stateOfCharge;
+    int numberOfSamples;
+    ReadBMSParametersFromInputFile(temperature, stateOfCharge, numberOfSamples);
+    printBMSParamsOnConsole(temperature, stateOfCharge, numberOfSamples);
+    return SenderStatus::SUCCESS;
+}
