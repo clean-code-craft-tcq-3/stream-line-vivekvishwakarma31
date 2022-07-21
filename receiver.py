ba3c6db9749b1e4b1c1756d2f29af6c002a14a12
tejeshsai <48875590+tejeshsai@users.noreply.github.com>
2022-07-21 20:45:20 +0530

    Testing print on receiver

--- receiver.py
+++ receiver.py
@@ -1,1 +1,7 @@
-print(input())
+if __name__ == "__main__":
+    while True:
+        try:
+            input_line = input()
+            print(input_line)
+        except EOFError:
+            break
