1c2c0838910467fed7a121ed9a70b42d95a321b4
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-08-22 18:41:04 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include<iterator>
+#include <stdio.h>
 #define TOTALSAMPLES 50
 
 enum SenderStatus{
@@ -13,4 +14,5 @@ enum SenderStatus{
 
 SenderStatus ReadBMSParametersFromInputFile(std::vector<float> &temperature, std::vector<float> &stateOfCharge, int &numberOfSamples);
 SenderStatus printBMSParamsOnConsole(std::vector<float> &temperature, std::vector<float> &stateOfCharge, int &numberOfSamples);
+SenderStatus WriteBMSParamsToFile(std::vector<float> &temperature, std::vector<float> &stateOfCharge, int &numberOfSamples);
 SenderStatus SendBMSParameters();
